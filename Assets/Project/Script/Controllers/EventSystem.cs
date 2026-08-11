18ff0610fc14183d5be160c6395c8f652dca686e
Artur Pinheiro <artur.assuncaopinheiro@gmail.com>
2026-08-11 12:02:41 -0300

    Added EventSystem

---
+++ Assets/Project/Script/Controllers/EventSystem.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace Gazeus.DesafioMatch3 {
+    public class EventSystem : MonoBehaviour {
+
+        public static Action OnTileDestroyed;
+    
+    }
+}
